123b0afc233788e5cfd91ca13822afe4e37e99ec
Laura Pigott <pigottlaura@gmail.com>
2016-03-03 21:25:42 +0000

    Experimenting with Twitter Access in Android Studio
    Following a tutorial on http://www.androidwarriors.com/2015/11/twitter-login-android-studio-example.html I can allow a user to login, and when they return to the app, their login name is logged out to the console.

---
+++ Prototyping_Components/androidStudio_03032016_a_TwitterAccess/TwitterAccess/app/src/main/java/com/pigottlaura/www/twitteraccess/MainActivity.java
@@ -0,0 +1,112 @@
+package com.pigottlaura.www.twitteraccess;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.User;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "huoLN2BllLtOzezay2ei07bzo";
+    private static final String TWITTER_SECRET = "k2OgK1XmjHLBMBRdM9KKyu86GS8wdIsv9Wbk9QOdzObXzHYsjb";
+
+    private TwitterLoginButton loginButton;
+    TextView textView;
+    TwitterSession session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_main);
+
+
+        textView = (TextView) findViewById(R.id.tv_username);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+
+
+                session = result.data;
+
+                String username = session.getUserName();
+                Long  userid = session.getUserId();
+
+                Log.d("Username = ", username);
+                textView.setText("Hi " + username);
+
+
+
+
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    void getUserData() {
+        Twitter.getApiClient(session).getAccountService()
+                .verifyCredentials(true, false, new Callback<User>() {
+
+                    @Override
+                    public void failure(TwitterException e) {
+
+                    }
+
+                    @Override
+                    public void success(Result<User> userResult) {
+
+                        User user = userResult.data;
+                        String twitterImage = user.profileImageUrl;
+
+                        try {
+                            Log.d("imageurl", user.profileImageUrl);
+                            Log.d("name", user.name);
+                            //Log.d("email",user.email);
+                            Log.d("des", user.description);
+                            Log.d("followers ", String.valueOf(user.followersCount));
+                            Log.d("createdAt", user.createdAt);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                });
+
+
+    }
+}
