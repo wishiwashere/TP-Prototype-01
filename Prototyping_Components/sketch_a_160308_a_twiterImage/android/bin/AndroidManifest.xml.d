437f695c3920977cb52b5a220254ca21835f175a
Laura Pigott <pigottlaura@gmail.com>
2016-03-08 09:58:50 +0000

    Tweeting Images to Twitter
     Now able to access the latest saved image and using it file path in a variable, you can share it to twitter. Fixed the switching of screens when searching and sharing.

---
+++ Prototyping_Components/sketch_a_160308_a_twiterImage/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\bin\AndroidManifest.xml : \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\AndroidManifest.xml \
---
+++ Prototyping_Components/sketch_a_160308_a_twiterImage/android/bin/Wish_I_Was_Here-release-unsigned.apk.d
@@ -0,0 +1,9 @@
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\bin\Wish_I_Was_Here-release-unsigned.apk : \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\bin\Wish_I_Was_Here.ap_ \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\bin\classes.dex \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\twitter4j-media-support-4.0.4.jar \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\ketai.jar \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\twitter4j-stream-4.0.4.jar \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\twitter4j-async-4.0.4.jar \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\twitter4j-core-4.0.4.jar \
+C:\Users\pigot\AppData\Local\Temp\android2883885493623974246sketch\libs\processing-core.jar \
