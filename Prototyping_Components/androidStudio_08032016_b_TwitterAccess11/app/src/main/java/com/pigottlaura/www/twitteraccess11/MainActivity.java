8fb1864c8e17fd4133eb75519dc84dcea683bfcb
Laura Pigott <pigottlaura@gmail.com>
2016-03-08 05:05:35 +0000

    Successfully running a processing sketch in Android Studio
    After restarting my attempt to replicate something similar to the tutorial on http://andymodlaphotography.blogspot.ie/2015/07/processing-sketches-using-android-studio.html I finally have a working app that runs a processing sketch (which for now just prints out Hello World, and allows you to draw on the screen).

---
+++ Prototyping_Components/androidStudio_08032016_b_TwitterAccess11/app/src/main/java/com/pigottlaura/www/twitteraccess11/MainActivity.java
@@ -0,0 +1,43 @@
+package com.pigottlaura.www.twitteraccess11;
+
+import android.os.Bundle;
+
+import processing.core.PApplet;
+
+
+public class MainActivity extends PApplet {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void settings() {
+        size(1920, 1080, OPENGL);
+        fullScreen();
+        println("Processing Settings Function Called");
+    }
+
+    @Override
+    public void setup() {
+        println("Processing Setup Funciton Called");
+        background(0);
+        strokeWeight(4);
+        textSize(width / 8);
+        textAlign(CENTER);
+        text("Hello World", width / 2, height / 2);
+    }
+
+    @Override
+    public void draw() {
+        //println("Processing Draw Function Called at frame " + frameCount);
+        stroke(237, 24, 223);
+
+        if (mousePressed) {
+            //println("Processing Mouse Pressed");
+            line(pmouseX, pmouseY, mouseX, mouseY);
+        }
+    }
+
+}
