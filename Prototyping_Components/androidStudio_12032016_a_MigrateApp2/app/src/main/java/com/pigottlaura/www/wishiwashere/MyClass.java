a47776559524083e85c74c5f4c95b18785e3959d
Laura Pigott <pigottlaura@gmail.com>
2016-03-12 22:14:31 +0000

    Testing if global variables carry
    Testing if global variables (within the Sketch class) carry over to classes which extend from it. They do

---
+++ Prototyping_Components/androidStudio_12032016_a_MigrateApp2/app/src/main/java/com/pigottlaura/www/wishiwashere/MyClass.java
@@ -0,0 +1,22 @@
+package com.pigottlaura.www.wishiwashere;
+
+import processing.core.PApplet;
+
+public class MyClass extends Sketch {
+    private PApplet p;
+    private String addText;
+
+    public MyClass(PApplet _p, String myText){
+        p = _p;
+        addText = myText;
+    }
+
+    public void show(){
+        println("MyClass show method called - " + addText);
+        println("MyNum = " + myNum);
+        p.fill(0);
+        p.textSize(20);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.text(addText, p.width/2, p.height/2 + 100);
+    }
+}
