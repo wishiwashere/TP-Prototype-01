dde91b4efe87ce659bb431be974b8eb10707c41e
Laura Pigott <pigottlaura@gmail.com>
2016-03-13 23:50:56 +0000

    Implemented adding to favourites

---
+++ Prototyping_Components/sketch_160313_a_addToFavourites/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\pigot\AppData\Local\Temp\android8445879777725236373sketch\bin\AndroidManifest.xml : \
+C:\Users\pigot\AppData\Local\Temp\android8445879777725236373sketch\AndroidManifest.xml \
