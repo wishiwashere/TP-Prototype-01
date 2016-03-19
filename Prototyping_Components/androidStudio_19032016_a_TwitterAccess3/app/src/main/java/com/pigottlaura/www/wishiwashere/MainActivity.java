544361408f9850fcf14d30958f9d8d27983c5010
Laura Pigott <pigottlaura@gmail.com>
2016-03-19 19:26:45 +0000

    Prototyping using Fabric.io with Processing
    Prototyping a method to use Fabric.io (to login to Twitter) in conjunction with a Processing sketch. Currently, I have created all of the code relating to Twitter in a new TwitterActivity. When the app runs, the user sees the MainActivity (which contains the Processing Sketch fragment). Once they tap on the screen, I use an intent to switch to the TwitterActivity. Technically, the user would then see that activity's screen, but as I wanted to avoid this I am using the preformClick() method on the twitter loginButton, so that a click is triggered on it just as the activity loads, so the user is taken directly to the Twitter authorisation page. Once the TwitterActivity recieves the data from the login, it stores then in public static variables, and then uses another intent to switch back to the MainActivity (i.e. the Processing Sketch). Within the Processing sketch, I am then printing out the values of these variables (username, userid, access token and secret token).

---
+++ Prototyping_Components/androidStudio_19032016_a_TwitterAccess3/app/src/main/java/com/pigottlaura/www/wishiwashere/MainActivity.java
@@ -0,0 +1,27 @@
+package com.pigottlaura.www.wishiwashere;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Setting the XML file which will determine the layout of this
+        // activity i.e. make the sketch appear full screen etc.
+        setContentView(R.layout.activity_main);
+
+        // Adding the Processing Sketch as a new Fragment (as in Processing
+        // 3.0, PApplet extends Fragment)
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new Sketch();
+        fragmentManager.beginTransaction()
+                .replace(R.id.processingSketch, fragment)
+                .commit();
+    }
+
+}
