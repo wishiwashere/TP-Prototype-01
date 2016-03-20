42c3306bf5251acba9ed863e3979a8deb4cc502e
Laura Pigott <pigottlaura@gmail.com>
2016-03-20 01:17:36 +0000

    Trying to incorporate Twitter login into main app
    Trying to incorporate Twitter login into the main app. Currently the app is crashing on resume due to a Fatal exception 'out of memory'. After some investigation I feel that this is because resuming the MainActivity is resetting the entire sketch, and that since it is trying to recreate all of the variables etc again there is not enough memory available. Will work on pausing the activity as opposed to stopping it completly.

---
+++ Prototyping_Components/androidStudio_19032016_a_TwitterAccess4/app/src/main/java/com/pigottlaura/www/wishiwashere/Screen.java
@@ -0,0 +1,126 @@
+package com.pigottlaura.www.wishiwashere;
+
+import processing.core.*;
+
+public class Screen extends Rectangle {
+    private Sketch sketch;
+
+    // Declaring the screenIcons array, which will be used to store the
+    // icons from each screen. This array will be initialised through the
+    // setScreenIcons() method, where each screen will pass in an array
+    // or the icons that are present on it's screen. The purpose of storing
+    // this array within the Screen class, as opposed to in each instance's
+    // own class, is that now we can deal with looping through, and calling
+    // methods on multiple icons of a screen only when their screen is being
+    // shown i.e. when showScreen() is being called
+    private Icon[] screenIcons = {};
+
+    // Creating and initialising the screenTitle property of the screen to be
+    // equal to an empty string. Each instance of the Screen class can then
+    // use the setScreenTitle() method to specify a custom title, which
+    // will then be used as the header text on that instance of the screen.
+    // If no title is specified, then header text will not be added to the
+    // screen
+    public String screenTitle = "";
+
+    // Creating protected constructors for the Screen class, so that they can
+    // only be accessed by classes which extend from this class
+
+  /*-------------------------------------- Constructor() ------------------------------------------------*/
+
+    protected Screen(Sketch _sketch) {
+        // Calling this class's super class (Screen) to create a screen using
+        // the default settings
+        super(_sketch);
+
+        sketch = _sketch;
+    }
+
+    protected Screen(Sketch _sketch, PImage img) {
+        // Calling this class's super class (Screen) to create a screen using
+        // the default settings along with a background image
+        super(_sketch, img);
+
+        sketch = _sketch;
+    }
+
+    protected Screen(Sketch _sketch, int col) {
+        // Calling this class's super class (Screen) to create a screen using
+        // the default settings, along with setting the colour as specified
+        super(_sketch, col);
+
+        sketch = _sketch;
+    }
+
+    protected Screen(Sketch _sketch, double x, double y, double w, double h, PImage img) {
+        super(_sketch, x, y, w, h, img);
+
+        sketch = _sketch;
+    }
+
+
+    protected Screen(Sketch _sketch, double x, double y, double w, double h, int col) {
+        super(_sketch, x, y, w, h, col);
+
+        sketch = _sketch;
+    }
+
+  /*-------------------------------------- drawScreen() ------------------------------------------------*/
+
+    // Creating a public method so that this screen can be displayed from
+    // within the main sketch
+    public void drawScreen() {
+        // Calling the show() method (as inherited from the Rectangle class)
+        // to display this screen's background
+        this.show();
+
+        // Checking if this screen has been given a title (i.e. if the contents
+        // of the screenTitle is at least one character long
+        if (this.screenTitle.length() > 0)
+        {
+            sketch.fill(0);
+            this.addText(this.screenTitle, sketch.appWidth/2, sketch.screenTitleY, sketch.appHeight * 0.08);
+        }
+
+        // Checking if this screen has any icons to be displayed
+        if (this.screenIcons.length > 0)
+        {
+            // Looping through each of the screen's icons
+            for (int i = 0; i < this.screenIcons.length; i++) {
+
+                // Calling the showIcon() method (as inherited from the Icon class)
+                // to display the icon on screen
+                this.screenIcons[i].showIcon();
+
+                // Checking if the mouse is currently pressed i.e. if a click has
+                // been detected
+                if (sketch.mousePressed)
+                {
+                    // Calling the checkMouseOver() method (as inherited from the Icon
+                    // class, to see if the mouse was over this icon when it was clicked
+                    this.screenIcons[i].checkMouseOver();
+                }
+            }
+        }
+    }
+
+  /*-------------------------------------- get() and set() ------------------------------------------------*/
+
+    protected void setScreenTitle(String title) {
+        sketch.println("FAV - Setting Screen Title to " + title);
+        // Setting the screenTitle to the title passed in by each screen. If no
+        // title is passed, this variable has already been initialised to an
+        // empty string above
+        this.screenTitle = title;
+    }
+
+    protected Icon[] getScreenIcons() {
+        return this.screenIcons;
+    }
+
+    protected void setScreenIcons(Icon[] icons) {
+        // Initialising the screenIcons array with the contents from the allIcons
+        // array that each screen will pass in
+        this.screenIcons = icons;
+    }
+}
---
+++ Prototyping_Components/androidStudio_19032016_a_TwitterAccess4/app/src/main/java/com/pigottlaura/www/wishiwashere/ShareSaveSuccessfulScreen.java
@@ -0,0 +1,40 @@
+package com.pigottlaura.www.wishiwashere;
+
+import processing.core.*;
+
+public class ShareSaveSuccessfulScreen extends Screen {
+    private Sketch sketch;
+
+    // Creating a public constructor for the TemplateScreen class, so that
+    // an instance of it can be declared in the main sketch
+    public ShareSaveSuccessfulScreen(Sketch _sketch, PImage bgImage) {
+
+        // Passing the color parametre to the super class (Screen), which will in
+        // turn call it's super class (Rectangle) and create a rectangle with the
+        // default values i.e. fullscreen, centered etc.
+        super(_sketch, bgImage);
+
+        sketch = _sketch;
+
+        // Setting the title of this screen. The screenTitle variable was also declared in this
+        // class's super class (Screen), so that it can be accessed when showing the screen
+        // (i.e can be displayed as the header text of the page). If no screenTitle were set,
+        // then no header text will appear on this page
+        this.setScreenTitle("");
+    }
+
+    // Creating a public showScreen method, which is called by the draw() funciton whenever this
+    // screen needs to be displayed
+    public void showScreen() {
+
+        // Calling the super class's (Screen) drawScreen() method, to display each of this screen's
+        // icons. This method will then in turn call it's super class's (Rectangle) method, to
+        // generate the size and background of the screen
+        this.drawScreen();
+        this.addText("Your postcard", sketch.iconCenterX, sketch.appHeight * 0.1);
+        this.addText("has been", sketch.iconCenterX, sketch.appHeight * 0.18);
+        this.addText("successfully", sketch.iconCenterX, sketch.appHeight * 0.26);
+        this.addText("shared & saved", sketch.iconCenterX, sketch.appHeight * 0.34);
+        this.addImage(sketch.loadImage("sharingScreenImage.png"), sketch.appWidth/2, sketch.appHeight * 0.6, sketch.appWidth * 0.8, sketch.appWidth * 0.4);
+    }
+}
