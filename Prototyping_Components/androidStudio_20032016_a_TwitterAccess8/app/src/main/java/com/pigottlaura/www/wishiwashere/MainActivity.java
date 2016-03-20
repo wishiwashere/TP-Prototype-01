e300b8766d0bc1554cc03275a3a0460b4427fd56
Laura Pigott <pigottlaura@gmail.com>
2016-03-20 21:51:27 +0000

    Possible success with Twitter in Main app
    By default the user now logs in to twitter first, and is then taken to the main sketch (as this is the only way I can currently guarantee the main sketch does not get rebuilt and thus there is no crash due to lack of memory). Not sure if this is working as I cannot test it on my device, getting Eiren to check it on her phone.

---
+++ Prototyping_Components/androidStudio_20032016_a_TwitterAccess8/app/src/main/java/com/pigottlaura/www/wishiwashere/MainActivity.java
@@ -0,0 +1,36 @@
+package com.pigottlaura.www.wishiwashere;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new Sketch();
+        fragmentManager.beginTransaction()
+                .replace(R.id.processingSketch, fragment)
+                .commit();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Main Activity", "resumed");
+
+        Log.d("Main Activity", "Main Activity resumed. twitterLogged in = " + TwitterLoginActivity.twitterLoggedIn);
+        /*
+        Log.d("Main Activity", "Twitter username = " + TwitterLoginActivity.twitterUserUsername);
+        Log.d("Main Activity", "userid = " + TwitterLoginActivity.twitterUserUserId);
+        Log.d("Main Activity", "Twitter access token = " + TwitterLoginActivity.twitterUserAccessToken);
+        Log.d("Main Activity", "Twitter secret token = " + TwitterLoginActivity.twitterUserSecretToken);
+        */
+    }
+}
