27e340b3b9f66b60a1f93dcfaea5a390204b3d89
Laura Pigott <pigottlaura@gmail.com>
2016-03-21 14:21:32 +0000

    Some success with server side image manipulation
    Having some success with server side image manipulation, using Node.js along with the 'jimp' and 'onecolor' packages. Currently, I have the green screen image of the girl loaded on the server, I am using jimp to open the image and run through it's pixels, and then using onecolor to cast the color of the pixels to the relevant values e.g. red, green and blue. I am then loading an image into the processing sketch using loadImage() and passing the url of localhost (as I am running the server locally using Visual Studio Code) and then displaying the resulting image in the sketch. The resulting image (as can be seen in finalImage.png) is extremely distorted (and I have not yet performed any keying to the green screen). All I have done is read in the color of the pixel, and then set the colour of the pixel back to it's own color. I am still unsure of how to send the live camera view images to the server, but I am concerned about how much this method will help us as currently it seems to take approximatley 6 seconds to process this one image.

--- Prototyping_Components/nodejs_21032016_ProcessingImages2/routes/index.js
+++ Prototyping_Components/nodejs_21032016_ProcessingImages2/routes/index.js
@@ -1,12 +1,65 @@
 var express = require('express');
 var router = express.Router();
+
+var onecolor = require('onecolor');
 var Jimp = require('jimp');
 
-router.get('/:cameraImage', function(req, res, next) {
-    Jimp.read(req.params.cameraImage, function(err, image){
-        console.log("Image received - " + req.params.cameraImage);
+var keyedImage;
+
+Jimp.read("./girlGreenScreen.jpg", function(err, cameraImage){
+    console.log("Image loaded");
+    cameraImage.rgba(true);
+    
+    cameraImage.scan(0, 0, cameraImage.bitmap.width, cameraImage.bitmap.height, function (x, y, idx) {
+
+        var pixelColorObject = Jimp.intToRGBA(cameraImage.getPixelColor(x, y));
+        var pixelColor = "rgb(" + pixelColorObject.r + ", " + pixelColorObject.g + ", " + pixelColorObject.b + ")";
+        //var pixelHue = onecolor(pixelColor).hue() * 360;
+        //var pixelSaturation = onecolor(pixelColor).saturation() * 100;
+        //var pixelBrightness = onecolor(pixelColor).lightness() * 100;
+        var pixelRed = onecolor(pixelColor).red() * 255;
+        var pixelGreen = onecolor(pixelColor).green() * 255;
+        var pixelBlue = onecolor(pixelColor).blue() * 225;
+        
+        var newColor = Jimp.rgbaToInt(pixelRed , pixelGreen, pixelBlue, 255);
+        
+        cameraImage.setPixelColor(newColor, x, y);
     });
-    res.send("Thank you :)");
+    cameraImage.write("finalImage.png");
+    console.log("DONE");
+    
+   
+    //console.log(onecolor(Jimp.intToRGBA(image.getPixelColor(1, 1))).lightness());
+    //console.log(Jimp.intToRGBA(image.getPixelColor(1, 1)));
+});
+
+//router.get('/:cameraImage', function(req, res, next) {
+router.get('/', function(req, res, next) {
+    /*
+    fs.readFile("./girlGreenScreen.jpg", function(err, data){
+        if(err) {
+            console.log("Unable to load image - " + err);
+        } else {
+            console.log("Image loaded");
+            
+        }
+    });
+    */
+    /*
+    Jimp.read(req.params.cameraImage.buffer, function(err, image){
+        console.log("Image received - " + req.params.cameraImage);
+        image.write("testImage.png", function(err){
+            if(err){
+                console.log("Could not save");
+            } else {
+                console.log("Completed Save");
+            }
+        })
+    });
+    */
+    
+    
+    res.download("./finalImage.png");
 });
 
 module.exports = router;
