0bac2827586799f50ce8218884bd54a2e318b0ef
Laura Pigott <pigottlaura@gmail.com>
2016-04-02 12:49:57 +0100

    Making copy of ProcessingImages6 to work from

---
+++ Prototyping_Components/nodejs_02042016_ProcessingImages6/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var RemoveGreenScreen = require("../custom_modules/RemoveGreenScreen");
+
+router.get('/:message', function (req, res, next) {
+    console.log(req.params.message);
+    res.send("Thank you :)");
+});
+
+router.get('/', function (req, res, next) {
+    // Increasing the default timeout to 15 seconds, as the greenscreening can take alot of time
+    // and I don't want the request to timeout while it is processing
+    req.setTimeout(15000, function (err) {
+        console.log("Server timed out " + err);
+    });
+    
+    // Calling the HD version of removing the green screen (can switch to SD by changing the 
+    // method to SD). HD Takes approximatley 8 seconds to process, while SD take 5 seconds
+    RemoveGreenScreen.HD("./girlGreenScreen.jpg");
+    
+    // Sending the resulting image back to the user in the response
+    res.download("./KeyedImage-HD.png");
+});
+
+module.exports = router;
